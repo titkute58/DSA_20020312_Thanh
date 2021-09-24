01fc2a969a166268dee53acef3a2b495372517d7
titkute58 <dohuytit12345@gmail.com>
2021-09-24 21:02:56 +0700

    Addition

---
+++ IntroJava/First_Chapter.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class First_Chapter {
+    public static void main(String[] args) {
+        //Cau 1
+        /*
+        for(int i = 0;i < 10;i++){
+            System.out.println("Hello World!");
+        }
+         */
+        //HiThree
+        //System.out.println("Hi " + args[0] + " " + args[1] + " " + args[2]);
+        //Cau 3
+        /*
+        System.out.println("****       **    *************     **************");
+        System.out.println("*****      **          **                 *     ");
+        System.out.println("**  **     **          **                 *     ");
+        System.out.println("**   **    **          **                 *     ");
+        System.out.println("**    **   **          **                 *     ");
+        System.out.println("**     **  **          **                 *     ");
+        System.out.println("**      ** **          **                 *     ");
+        System.out.println("**       ****          **                 *     ");
+        System.out.println("**        ***          **                 *     ");
+         */
+    }
+}
